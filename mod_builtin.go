75d52b2600c2925381c87550371c3a7d4e694d6a
Konstantin Voykov <koykov0@gmail.com>
2020-05-21 00:10:03 +0300

    Make type assertion helpers for modifiers.

--- mod_builtin.go
+++ mod_builtin.go
@@ -1,70 +1,45 @@
 package dyntpl
 
-func modDefault(_ *Ctx, buf *interface{}, val interface{}, args []interface{}) error {
+func modDefault(_ *Ctx, buf *interface{}, val interface{}, args []interface{}) (err error) {
 	if len(args) == 0 {
-		return ErrModNoArgs
+		err = ErrModNoArgs
+		return
 	}
-	b := false
-	switch val.(type) {
-	case int:
-		b = val.(int) == 0
-	case *int:
-		b = *val.(*int) == 0
-	case int8:
-		b = val.(int8) == 0
-	case *int8:
-		b = *val.(*int8) == 0
-	case int16:
-		b = val.(int16) == 0
-	case *int16:
-		b = *val.(*int16) == 0
-	case int32:
-		b = val.(int32) == 0
-	case *int32:
-		b = *val.(*int32) == 0
-	case int64:
-		b = val.(int64) == 0
-	case *int64:
-		b = *val.(*int64) == 0
-	case uint:
-		b = val.(uint) == 0
-	case *uint:
-		b = *val.(*uint) == 0
-	case uint8:
-		b = val.(uint8) == 0
-	case *uint8:
-		b = *val.(*uint8) == 0
-	case uint16:
-		b = val.(uint16) == 0
-	case *uint16:
-		b = *val.(*uint16) == 0
-	case uint32:
-		b = val.(uint32) == 0
-	case *uint32:
-		b = *val.(*uint32) == 0
-	case uint64:
-		b = val.(uint64) == 0
-	case *uint64:
-		b = *val.(*uint64) == 0
-	case float32:
-		b = val.(float32) == 0
-	case *float32:
-		b = *val.(*float32) == 0
-	case float64:
-		b = val.(float64) == 0
-	case *float64:
-		b = *val.(*float64) == 0
-	case []byte:
-		b = len(val.([]byte)) == 0
-	case *[]byte:
-		b = len(*val.(*[]byte)) == 0
-	case string:
-		b = len(val.(string)) == 0
-	case *string:
-		b = len(*val.(*string)) == 0
+	if i, ok := ModInt(val); ok {
+		if i == 0 {
+			*buf = args[0]
+			return
+		}
 	}
-	if b {
-		*buf = args[0]
+	if u, ok := ModUint(val); ok {
+		if u == 0 {
+			*buf = args[0]
+			return
+		}
+	}
+	if f, ok := ModFloat(val); ok {
+		if f == 0 {
+			*buf = args[0]
+			return
+		}
+	}
+	if b, ok := ModBytes(val); ok {
+		if len(b) == 0 {
+			*buf = args[0]
+			return
+		}
+	}
+	if s, ok := ModStr(val); ok {
+		if len(s) == 0 {
+			*buf = args[0]
+			return
+		}
+	}
+	if b, ok := ModBool(val); ok {
+		if !b {
+			*buf = args[0]
+			return
+		}
 	}
 	return nil
 }
