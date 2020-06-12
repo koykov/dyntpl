4fa7c38b036d8080448a4dc25de8fd7b4d9dd88f
Konstantin Voykov <koykov0@gmail.com>
2020-06-12 23:24:26 +0300

    Comment init() code.

--- init.go
+++ init.go
@@ -3,15 +3,18 @@ package dyntpl
 import "github.com/koykov/any2bytes"
 
 func init() {
+	// Register simple builtin modifiers.
 	RegisterModFn("default", "def", modDefault)
 	RegisterModFn("ifThen", "if", modIfThen)
 	RegisterModFn("ifThenElse", "ifel", modIfThenElse)
 
+	// Register builtin escape/quote modifiers.
 	RegisterModFn("jsonEscape", "je", modJsonEscape)
 	RegisterModFn("jsonQuote", "jq", modJsonQuote)
 	RegisterModFn("htmlEscape", "he", modHtmlEscape)
 	RegisterModFn("urlEncode", "ue", modUrlEncode)
 
+	// Register builtin round modifiers.
 	RegisterModFn("round", "round", modRound)
 	RegisterModFn("roundPrec", "roundp", modRoundPrec)
 	RegisterModFn("ceil", "ceil", modCeil)
@@ -19,9 +22,11 @@ func init() {
 	RegisterModFn("floor", "floor", modFloor)
 	RegisterModFn("floorPrec", "floorp", modFloorPrec)
 
+	// Register builtin condition helpers.
 	RegisterCondFn("lenEq0", condLenEq0)
 	RegisterCondFn("lenGt0", condLenGt0)
 	RegisterCondFn("lenGtq0", condLenGtq0)
 
+	// Register byte buffer to bytes function.
 	any2bytes.RegisterAnyToBytesFn(ByteBufToBytes)
 }
