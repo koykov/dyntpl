d3e4e777467cfe799b4c237d601db47d4c209326
Konstantin Voykov <koykov0@gmail.com>
2020-06-12 23:29:57 +0300

    Finish comment of context code.

--- ctx_buf.go
+++ ctx_buf.go
@@ -5,8 +5,10 @@ import (
 	"github.com/koykov/fastconv"
 )
 
+// Primitive byte buffer to use in context objects and modifiers/helpers.
 type ByteBuf []byte
 
+// Conversion to bytes function.
 func ByteBufToBytes(dst []byte, val interface{}) ([]byte, error) {
 	if b, ok := val.(*ByteBuf); ok {
 		dst = append(dst, *b...)
@@ -15,24 +17,30 @@ func ByteBufToBytes(dst []byte, val interface{}) ([]byte, error) {
 	return dst, any2bytes.ErrUnknownType
 }
 
+// Reset length of the buffer.
 func (b *ByteBuf) Reset() *ByteBuf {
 	*b = (*b)[:0]
 	return b
 }
 
+// Get contents of the buffer.
 func (b *ByteBuf) Bytes() []byte {
 	return *b
 }
 
+// Get contents of the buffer as string.
 func (b *ByteBuf) String() string {
 	return fastconv.B2S(*b)
 }
 
+// Write bytes to the buffer.
 func (b *ByteBuf) Write(p []byte) *ByteBuf {
 	*b = append(*b, p...)
 	return b
 }
 
+// Write single byte.
+//
 // Use suffix "B" instead of "Byte" to avoid warning of incorrect naming of canonical methods:
 // > Method 'WriteByte(byte)' should have signature 'WriteByte(byte) error'
 // For more info see https://github.com/golang/tools/blob/master/go/analysis/passes/stdmethods/stdmethods.go#L63
@@ -41,21 +49,25 @@ func (b *ByteBuf) WriteB(p byte) *ByteBuf {
 	return b
 }
 
+// Write string to the buffer.
 func (b *ByteBuf) WriteStr(s string) *ByteBuf {
 	*b = append(*b, s...)
 	return b
 }
 
+// Write integer value to the buffer.
 func (b *ByteBuf) WriteInt(i int64) *ByteBuf {
 	*b, _ = any2bytes.IntToBytes(*b, i)
 	return b
 }
 
+// Write float value to the boffer.
 func (b *ByteBuf) WriteFloat(f float64) *ByteBuf {
 	*b, _ = any2bytes.FloatToBytes(*b, f)
 	return b
 }
 
+// Write boolean value to the buffer.
 func (b *ByteBuf) WriteBool(v bool) *ByteBuf {
 	*b, _ = any2bytes.BoolToBytes(*b, v)
 	return b
--- ctx_pool.go
+++ ctx_pool.go
@@ -2,12 +2,16 @@ package dyntpl
 
 import "sync"
 
+// Context pool.
 type CtxPool struct {
 	p sync.Pool
 }
 
+// Default instance of context pool.
+// You may use it directly as dyntpl.CP.Get()/Put() or using functions AcquireCtx()/ReleaseCtx().
 var CP CtxPool
 
+// Get context object from the pool or make new object if pool is empty.
 func (p *CtxPool) Get() *Ctx {
 	v := p.p.Get()
 	if v != nil {
@@ -18,15 +22,18 @@ func (p *CtxPool) Get() *Ctx {
 	return NewCtx()
 }
 
+// Put the object to the pool.
 func (p *CtxPool) Put(ctx *Ctx) {
 	ctx.Reset()
 	p.p.Put(ctx)
 }
 
+// Get object from the default context pool.
 func AcquireCtx() *Ctx {
 	return CP.Get()
 }
 
+// Put object back to default pool.
 func ReleaseCtx(ctx *Ctx) {
 	CP.Put(ctx)
 }
