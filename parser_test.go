56632a71c1cc5450788a89ac714b362e5bd550c5
Konstantin Voykov <koykov0@gmail.com>
2021-09-16 19:44:18 +0300

    Testing issue: improve parser tests (allow to run separate test from stages).

--- parser_test.go
+++ parser_test.go
@@ -11,42 +11,42 @@ import (
 func TestParser(t *testing.T) {
 	type stage struct {
 		key string
-		fn  func(*testing.T, *stage)
 		origin,
 		expect []byte
+		raw bool
 		err error
 	}
 
-	tst := func(t *testing.T, stage *stage) {
-		stage.expect, _ = ioutil.ReadFile("testdata/parser/" + stage.key + ".xml")
-		tree, _ := Parse(stage.origin, false)
+	tst := func(t *testing.T, key string, origin []byte, _ error) {
+		expect, _ := ioutil.ReadFile("testdata/parser/" + key + ".xml")
+		tree, _ := Parse(origin, false)
 		r := tree.HumanReadable()
-		if !bytes.Equal(r, stage.expect) {
-			t.Errorf("%s test failed\nexp: %s\ngot: %s", stage.key, string(stage.expect), string(r))
+		if !bytes.Equal(r, expect) {
+			t.Errorf("%s test failed\nexp: %s\ngot: %s", key, string(expect), string(r))
 		}
 	}
-	tstRaw := func(t *testing.T, stage *stage) {
-		stage.expect, _ = ioutil.ReadFile("testdata/parser/" + stage.key + ".txt")
-		stage.expect = bytealg.Trim(stage.expect, []byte("\n"))
-		p := &Parser{tpl: stage.origin}
+	tstRaw := func(t *testing.T, key string, origin []byte, _ error) {
+		expect, _ := ioutil.ReadFile("testdata/parser/" + key + ".txt")
+		expect = bytealg.Trim(expect, []byte("\n"))
+		p := &Parser{tpl: origin}
 		p.cutComments()
 		p.cutFmt()
-		if !bytes.Equal(stage.expect, p.tpl) {
-			t.Errorf("%s test raw failed\nexp: %s\ngot: %s", stage.key, string(stage.expect), string(p.tpl))
+		if !bytes.Equal(expect, p.tpl) {
+			t.Errorf("%s test raw failed\nexp: %s\ngot: %s", key, string(expect), string(p.tpl))
 		}
 	}
-	tstErr := func(t *testing.T, stage *stage) {
-		_, err := Parse(stage.origin, false)
-		if err != stage.err {
-			t.Errorf("%s test error failed\nexp err: %s\ngot: %s", stage.key, stage.err, err)
+	tstErr := func(t *testing.T, key string, origin []byte, err error) {
+		_, err1 := Parse(origin, false)
+		if err != err1 {
+			t.Errorf("%s test error failed\nexp err: %s\ngot: %s", key, err, err1)
 		}
 	}
 
 	stages := []stage{
-		{key: "cutComments", fn: tstRaw},
-		{key: "cutFmt", fn: tstRaw},
+		{key: "cutComments", raw: true},
+		{key: "cutFmt", raw: true},
 		{key: "printVar"},
-		{key: "unexpectedEOF", fn: tstErr, err: ErrUnexpectedEOF},
+		{key: "unexpectedEOF", err: ErrUnexpectedEOF},
 		{key: "prefixSuffix"},
 		{key: "exit"},
 		{key: "mod"},
@@ -76,11 +76,16 @@ func TestParser(t *testing.T) {
 	}
 
 	for _, s := range stages {
-		fn := s.fn
-		if fn == nil {
-			fn = tst
-		}
-		s.origin, _ = ioutil.ReadFile("testdata/parser/" + s.key + ".tpl")
-		t.Run(s.key, func(t *testing.T) { fn(t, &s) })
+		t.Run(s.key, func(t *testing.T) {
+			s.origin, _ = ioutil.ReadFile("testdata/parser/" + s.key + ".tpl")
+			switch {
+			case s.raw:
+				tstRaw(t, s.key, s.origin, s.err)
+			case s.err != nil:
+				tstErr(t, s.key, s.origin, s.err)
+			default:
+				tst(t, s.key, s.origin, s.err)
+			}
+		})
 	}
 }
