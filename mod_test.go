038025fab63eca9485d22b3ab68235410a34c78e
Konstantin Voykov <koykov0@gmail.com>
2021-09-18 00:22:00 +0300

    Testing issues: mod args type.

--- mod_test.go
+++ mod_test.go
@@ -5,44 +5,46 @@ import (
 	"testing"
 )
 
+type modArgs map[string]interface{}
+
 func TestMod(t *testing.T) {
 	t.Run("modDefault", func(t *testing.T) { testMod(t) })
 	t.Run("modDefaultStatic", func(t *testing.T) { testMod(t) })
 	t.Run("modDefault1", func(t *testing.T) { testMod(t) })
-	t.Run("modJSONEscape", func(t *testing.T) { testModWA(t, map[string]interface{}{"userName": `Foo"bar`}) })
-	t.Run("modJSONEscapeShort", func(t *testing.T) { testModWA(t, map[string]interface{}{"userName": `Foo"bar`}) })
+	t.Run("modJSONEscape", func(t *testing.T) { testModWA(t, modArgs{"userName": `Foo"bar`}) })
+	t.Run("modJSONEscapeShort", func(t *testing.T) { testModWA(t, modArgs{"userName": `Foo"bar`}) })
 	t.Run("modJSONEscapeDbl", func(t *testing.T) {
-		testModWA(t, map[string]interface{}{"valueWithQuotes": `He said: "welcome friend"`})
+		testModWA(t, modArgs{"valueWithQuotes": `He said: "welcome friend"`})
 	})
-	t.Run("modJSONQuoteShort", func(t *testing.T) { testModWA(t, map[string]interface{}{"userName": `Foo"bar`}) })
+	t.Run("modJSONQuoteShort", func(t *testing.T) { testModWA(t, modArgs{"userName": `Foo"bar`}) })
 	t.Run("modHtmlEscape", func(t *testing.T) {
-		testModWA(t, map[string]interface{}{
+		testModWA(t, modArgs{
 			"title": `<h1>Go is an open source programming language that makes it easy to build <strong>simple<strong>, <strong>reliable</strong>, and <strong>efficient</strong> software.</h1>`,
 			"text":  `Visit >`,
 		})
 	})
 	t.Run("modHtmlEscapeShort", func(t *testing.T) {
-		testModWA(t, map[string]interface{}{
+		testModWA(t, modArgs{
 			"title": `<h1>Go is an open source programming language that makes it easy to build <strong>simple<strong>, <strong>reliable</strong>, and <strong>efficient</strong> software.</h1>`,
 			"text":  `Visit >`,
 		})
 	})
 	t.Run("modLinkEscape", func(t *testing.T) {
-		testModWA(t, map[string]interface{}{"link": `http://x.com/link-with-"-and space-symbol`})
+		testModWA(t, modArgs{"link": `http://x.com/link-with-"-and space-symbol`})
 	})
 	t.Run("modURLEncode", func(t *testing.T) {
-		testModWA(t, map[string]interface{}{"url": `https://golang.org/src/net/url/url.go#L100`})
+		testModWA(t, modArgs{"url": `https://golang.org/src/net/url/url.go#L100`})
 	})
 	t.Run("modURLEncode2", func(t *testing.T) {
-		testModWA(t, map[string]interface{}{"url": `https://golang.org/src/net/url/url.go#L100`})
+		testModWA(t, modArgs{"url": `https://golang.org/src/net/url/url.go#L100`})
 	})
 	t.Run("modURLEncode3", func(t *testing.T) {
-		testModWA(t, map[string]interface{}{"url": `https://golang.org/src/net/url/url.go#L100`})
+		testModWA(t, modArgs{"url": `https://golang.org/src/net/url/url.go#L100`})
 	})
-	t.Run("modIfThen", func(t *testing.T) { testModWA(t, map[string]interface{}{"allow": true}) })
-	t.Run("modIfThenElse", func(t *testing.T) { testModWA(t, map[string]interface{}{"logged": true, "userName": "foobar"}) })
+	t.Run("modIfThen", func(t *testing.T) { testModWA(t, modArgs{"allow": true}) })
+	t.Run("modIfThenElse", func(t *testing.T) { testModWA(t, modArgs{"logged": true, "userName": "foobar"}) })
 	t.Run("modRound", func(t *testing.T) {
-		testModWA(t, map[string]interface{}{
+		testModWA(t, modArgs{
 			"f0": 7.243242,
 			"f1": 3.1415,
 			"f2": 11.39,
@@ -57,7 +59,7 @@ func testMod(t *testing.T) {
 	testTpl(t)
 }
 
-func testModWA(t *testing.T, args map[string]interface{}) {
+func testModWA(t *testing.T, args modArgs) {
 	key := getTBName(t)
 	st := getStage(key)
 	if st == nil {
@@ -82,40 +84,40 @@ func BenchmarkMod(b *testing.B) {
 	b.Run("modDefault", func(b *testing.B) { benchMod(b) })
 	b.Run("modDefaultStatic", func(b *testing.B) { benchMod(b) })
 	b.Run("modDefault1", func(b *testing.B) { benchMod(b) })
-	b.Run("modJSONEscape", func(b *testing.B) { benchModWA(b, map[string]interface{}{"userName": `Foo"bar`}) })
-	b.Run("modJSONEscapeShort", func(b *testing.B) { benchModWA(b, map[string]interface{}{"userName": `Foo"bar`}) })
+	b.Run("modJSONEscape", func(b *testing.B) { benchModWA(b, modArgs{"userName": `Foo"bar`}) })
+	b.Run("modJSONEscapeShort", func(b *testing.B) { benchModWA(b, modArgs{"userName": `Foo"bar`}) })
 	b.Run("modJSONEscapeDbl", func(b *testing.B) {
-		benchModWA(b, map[string]interface{}{"valueWithQuotes": `He said: "welcome friend"`})
+		benchModWA(b, modArgs{"valueWithQuotes": `He said: "welcome friend"`})
 	})
-	b.Run("modJSONQuoteShort", func(b *testing.B) { benchModWA(b, map[string]interface{}{"userName": `Foo"bar`}) })
+	b.Run("modJSONQuoteShort", func(b *testing.B) { benchModWA(b, modArgs{"userName": `Foo"bar`}) })
 	b.Run("modHtmlEscape", func(b *testing.B) {
-		benchModWA(b, map[string]interface{}{
+		benchModWA(b, modArgs{
 			"title": `<h1>Go is an open source programming language that makes it easy to build <strong>simple<strong>, <strong>reliable</strong>, and <strong>efficient</strong> software.</h1>`,
 			"text":  `Visit >`,
 		})
 	})
 	b.Run("modHtmlEscapeShort", func(b *testing.B) {
-		benchModWA(b, map[string]interface{}{
+		benchModWA(b, modArgs{
 			"title": `<h1>Go is an open source programming language that makes it easy to build <strong>simple<strong>, <strong>reliable</strong>, and <strong>efficient</strong> software.</h1>`,
 			"text":  `Visit >`,
 		})
 	})
 	b.Run("modLinkEscape", func(b *testing.B) {
-		benchModWA(b, map[string]interface{}{"link": `http://x.com/link-with-"-and space-symbol`})
+		benchModWA(b, modArgs{"link": `http://x.com/link-with-"-and space-symbol`})
 	})
 	b.Run("modURLEncode", func(b *testing.B) {
-		benchModWA(b, map[string]interface{}{"url": `https://golang.org/src/net/url/url.go#L100`})
+		benchModWA(b, modArgs{"url": `https://golang.org/src/net/url/url.go#L100`})
 	})
 	b.Run("modURLEncode2", func(b *testing.B) {
-		benchModWA(b, map[string]interface{}{"url": `https://golang.org/src/net/url/url.go#L100`})
+		benchModWA(b, modArgs{"url": `https://golang.org/src/net/url/url.go#L100`})
 	})
 	b.Run("modURLEncode3", func(b *testing.B) {
-		benchModWA(b, map[string]interface{}{"url": `https://golang.org/src/net/url/url.go#L100`})
+		benchModWA(b, modArgs{"url": `https://golang.org/src/net/url/url.go#L100`})
 	})
-	b.Run("modIfThen", func(b *testing.B) { benchModWA(b, map[string]interface{}{"allow": true}) })
-	b.Run("modIfThenElse", func(b *testing.B) { benchModWA(b, map[string]interface{}{"logged": true, "userName": "foobar"}) })
+	b.Run("modIfThen", func(b *testing.B) { benchModWA(b, modArgs{"allow": true}) })
+	b.Run("modIfThenElse", func(b *testing.B) { benchModWA(b, modArgs{"logged": true, "userName": "foobar"}) })
 	b.Run("modRound", func(b *testing.B) {
-		benchModWA(b, map[string]interface{}{
+		benchModWA(b, modArgs{
 			"f0": 7.243242,
 			"f1": 3.1415,
 			"f2": 11.39,
@@ -130,7 +132,7 @@ func benchMod(b *testing.B) {
 	benchTpl(b)
 }
 
-func benchModWA(b *testing.B, args map[string]interface{}) {
+func benchModWA(b *testing.B, args modArgs) {
 	key := getTBName(b)
 
 	st := getStage(key)
