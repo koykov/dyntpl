07e5d52392c8ae4c062774e546109a71c4cb79ca
Konstantin Voykov <koykov0@gmail.com>
2023-02-12 23:48:08 +0200

    Implement cssEscape modifier; cover it with test/bench.

--- mod_test.go
+++ mod_test.go
@@ -31,6 +31,7 @@ func TestMod(t *testing.T) {
 	})
 	t.Run("modAttrEscape", func(t *testing.T) { testModWA(t, modArgs{"var1": "foo&<>\"'`!@$%()=+{}[]#;bar"}) })
 	t.Run("modAttrEscapeMB", func(t *testing.T) { testModWA(t, modArgs{"var1": "Привет мир😀!"}) })
+	t.Run("modCSSEscape", func(t *testing.T) { testModWA(t, modArgs{"var1": "<>'\"&Ā,._aAzZ09 !😀"}) })
 	t.Run("modLinkEscape", func(t *testing.T) {
 		testModWA(t, modArgs{"link": `http://x.com/link-with-"-and space-symbol`})
 	})
@@ -106,6 +107,7 @@ func BenchmarkMod(b *testing.B) {
 	})
 	b.Run("modAttrEscape", func(b *testing.B) { benchModWA(b, modArgs{"var1": "foo&<>\"'`!@$%()=+{}[]#;bar"}) })
 	b.Run("modAttrEscapeMB", func(b *testing.B) { benchModWA(b, modArgs{"var1": "Привет мир😀!"}) })
+	b.Run("modCSSEscape", func(b *testing.B) { benchModWA(b, modArgs{"var1": "<>'\"&Ā,._aAzZ09 !😀"}) })
 	b.Run("modLinkEscape", func(b *testing.B) {
 		benchModWA(b, modArgs{"link": `http://x.com/link-with-"-and space-symbol`})
 	})
