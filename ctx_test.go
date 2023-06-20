8e1e57185e5e72bf6a6d5395b087cc6da5043879
Konstantin Voykov <koykov0@gmail.com>
2023-06-20 19:10:22 +0300

    Add support of deferred functions.

--- ctx_test.go
+++ ctx_test.go
@@ -1,6 +1,7 @@
 package dyntpl
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/koykov/inspector/testobj"
@@ -50,7 +51,7 @@ func TestCtx(t *testing.T) {
 		raw any
 	)
 	ctx := NewCtx()
-	ctx.Set("obj", testO, &ins)
+	ctx.Set("obj", testO, ins)
 
 	t.Run("get", func(t *testing.T) {
 		raw = ctx.Get("obj.Id")
@@ -67,6 +68,46 @@ func TestCtx(t *testing.T) {
 		}
 		if *raw.(*float64) != 9000 {
 			t.Error("ctx get mismatch: obj.Finance.Balance")
+		}
+	})
+	t.Run("defer", func(t *testing.T) {
+		var c int
+		const n = 10
+		for i := 0; i < n; i++ {
+			ctx.Defer(func() error {
+				c++
+				return nil
+			})
+		}
+		_ = ctx.defer_()
+		if c != n {
+			t.Errorf("ctx defer mismatch: need %d, got %d", n, c)
+		}
+	})
+	t.Run("defer pool", func(t *testing.T) {
+		type x struct {
+			payload []byte
+		}
+		p := sync.Pool{New: func() any {
+			return &x{}
+		}}
+		var a, r int
+		var ctx_ Ctx
+		const n = 10
+		for i := 0; i < n; i++ {
+			raw_ := p.Get()
+			a++
+			x_ := raw_.(*x)
+			ctx_.Defer(func() error {
+				x_.payload = x_.payload[:0]
+				p.Put(x_)
+				r++
+				return nil
+			})
+		}
+		_ = ctx_.defer_()
+		if a != r {
+			t.Errorf("ctx defer pool mismatch: need %d/%d, got %d/%d", n, n, a, r)
 		}
 	})
 }
@@ -96,6 +137,44 @@ func BenchmarkCtx(b *testing.B) {
 			benchCtx(b, ctx, &raw)
 
 			ReleaseCtx(ctx)
+		}
+	})
+	b.Run("defer", func(b *testing.B) {
+		b.ReportAllocs()
+		ctx := NewCtx()
+		var c int
+		for i := 0; i < b.N; i++ {
+			ctx.Reset()
+			c = 0
+			ctx.Defer(func() error {
+				c++
+				return nil
+			})
+			_ = ctx.defer_()
+			if c != 1 {
+				b.Errorf("ctx defer mismatch: need %d, got %d", 1, c)
+			}
+		}
+	})
+	b.Run("defer pool", func(b *testing.B) {
+		b.ReportAllocs()
+		type x struct {
+			payload []byte
+		}
+		p := sync.Pool{New: func() any {
+			return &x{}
+		}}
+		var ctx_ Ctx
+		for i := 0; i < b.N; i++ {
+			ctx_.Reset()
+			raw_ := p.Get()
+			x_ := raw_.(*x)
+			ctx_.Defer(func() error {
+				x_.payload = x_.payload[:0]
+				p.Put(x_)
+				return nil
+			})
+			_ = ctx_.defer_()
 		}
 	})
 }
