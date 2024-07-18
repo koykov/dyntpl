14eb6e87a2e78a32be75aecf0d68b879941a4d8e
Konstantin Voykov <koykov0@gmail.com>
2024-07-18 19:28:17 +0300

    docgen: render conditions (markdown); beautify output.

--- docgen.go
+++ docgen.go
@@ -70,6 +70,37 @@ func writeDocgenMarkdown(w io.Writer) error {
 		}
 	}
 
+	_, _ = w.Write([]byte("## Conditions\n\n"))
+	for i := 0; i < len(condBuf); i++ {
+		tuple := &condBuf[i]
+		_, _ = w.Write([]byte("### "))
+		_, _ = w.Write([]byte(tuple.id))
+		_, _ = w.Write([]byte("\n\n"))
+		if len(tuple.params) > 0 {
+			_, _ = w.Write([]byte("Params:\n"))
+			for j := 0; j < len(tuple.params); j++ {
+				param := &tuple.params[j]
+				_, _ = w.Write([]byte("* `" + param.param + "`"))
+				if len(param.desc) > 0 {
+					_, _ = w.Write([]byte(" " + param.desc))
+				}
+				_, _ = w.Write([]byte("\n"))
+			}
+			_, _ = w.Write([]byte("\n"))
+		}
+
+		if len(tuple.desc) > 0 {
+			_, _ = w.Write([]byte(tuple.desc))
+			_, _ = w.Write([]byte("\n\n"))
+		}
+
+		if len(tuple.example) > 0 {
+			_, _ = w.Write([]byte("Example:\n```\n"))
+			_, _ = w.Write([]byte(tuple.example))
+			_, _ = w.Write([]byte("\n```\n\n"))
+		}
+	}
+
 	return nil
 }
 
