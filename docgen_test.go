e7d83044cfa48d586059681f3e9e19e386e0d23d
Konstantin Voykov <koykov0@gmail.com>
2024-07-18 22:20:42 +0300

    docgen: test: use env variable.

--- docgen_test.go
+++ docgen_test.go
@@ -1,26 +1,29 @@
 package dyntpl
 
-import "testing"
-
-const docgenVerbose = true
+import (
+	"os"
+	"testing"
+)
 
 func TestDocgen(t *testing.T) {
+	generate := os.Getenv("DOCGEN_GENERATE") == "1"
+
 	t.Run("markdown", func(t *testing.T) {
 		docs, _ := Docgen(DocgenFormatMarkdown)
-		if docgenVerbose {
-			println(string(docs))
+		if generate {
+			_ = os.WriteFile("docgen.md", docs, 0644)
 		}
 	})
 	t.Run("html", func(t *testing.T) {
 		docs, _ := Docgen(DocgenFormatHTML)
-		if docgenVerbose {
-			println(string(docs))
+		if generate {
+			_ = os.WriteFile("docgen.html", docs, 0644)
 		}
 	})
 	t.Run("json", func(t *testing.T) {
 		docs, _ := Docgen(DocgenFormatJSON)
-		if docgenVerbose {
-			println(string(docs))
+		if generate {
+			_ = os.WriteFile("docgen.json", docs, 0644)
 		}
 	})
 }
