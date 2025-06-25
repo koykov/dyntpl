a318f67e357ccd185d51553c590d0f1678fb9bd4
Konstantin Voykov <koykov0@gmail.com>
2025-06-25 20:54:55 +0300

    type assert: user shorter switches

--- conv.go
+++ conv.go
@@ -16,27 +16,27 @@ type intConverter interface {
 // ConvInt tries to convert value to integer.
 func ConvInt(val any) (i int64, ok bool) {
 	ok = true
-	switch val.(type) {
+	switch x := val.(type) {
 	case int:
-		i = int64(val.(int))
+		i = int64(x)
 	case *int:
-		i = int64(*val.(*int))
+		i = int64(*x)
 	case int8:
-		i = int64(val.(int8))
+		i = int64(x)
 	case *int8:
-		i = int64(*val.(*int8))
+		i = int64(*x)
 	case int16:
-		i = int64(val.(int16))
+		i = int64(x)
 	case *int16:
-		i = int64(*val.(*int16))
+		i = int64(*x)
 	case int32:
-		i = int64(val.(int32))
+		i = int64(x)
 	case *int32:
-		i = int64(*val.(*int32))
+		i = int64(*x)
 	case int64:
-		i = val.(int64)
+		i = x
 	case *int64:
-		i = *val.(*int64)
+		i = *x
 	default:
 		ok = false
 	}
@@ -46,27 +46,27 @@ func ConvInt(val any) (i int64, ok bool) {
 // ConvUint tries to convert value to uint.
 func ConvUint(val any) (u uint64, ok bool) {
 	ok = true
-	switch val.(type) {
+	switch x := val.(type) {
 	case uint:
-		u = uint64(val.(uint))
+		u = uint64(x)
 	case *uint:
-		u = uint64(*val.(*uint))
+		u = uint64(*x)
 	case uint8:
-		u = uint64(val.(uint8))
+		u = uint64(x)
 	case *uint8:
-		u = uint64(*val.(*uint8))
+		u = uint64(*x)
 	case uint16:
-		u = uint64(val.(uint16))
+		u = uint64(x)
 	case *uint16:
-		u = uint64(*val.(*uint16))
+		u = uint64(*x)
 	case uint32:
-		u = uint64(val.(uint32))
+		u = uint64(x)
 	case *uint32:
-		u = uint64(*val.(*uint32))
+		u = uint64(*x)
 	case uint64:
-		u = val.(uint64)
+		u = x
 	case *uint64:
-		u = *val.(*uint64)
+		u = *x
 	default:
 		ok = false
 	}
@@ -76,15 +76,15 @@ func ConvUint(val any) (u uint64, ok bool) {
 // ConvFloat tries to convert value to float.
 func ConvFloat(val any) (f float64, ok bool) {
 	ok = true
-	switch val.(type) {
+	switch x := val.(type) {
 	case float32:
-		f = float64(val.(float32))
+		f = float64(x)
 	case *float32:
-		f = float64(*val.(*float32))
+		f = float64(*x)
 	case float64:
-		f = val.(float64)
+		f = x
 	case *float64:
-		f = *val.(*float64)
+		f = *x
 	default:
 		ok = false
 	}
@@ -94,15 +94,15 @@ func ConvFloat(val any) (f float64, ok bool) {
 // ConvBytes tries to convert value to bytes.
 func ConvBytes(val any) (b []byte, ok bool) {
 	ok = true
-	switch val.(type) {
+	switch x := val.(type) {
 	case []byte:
-		b = val.([]byte)
+		b = x
 	case *[]byte:
-		b = *val.(*[]byte)
+		b = *x
 	case bytebuf.Chain:
-		b = val.(bytebuf.Chain)
+		b = x
 	case *bytebuf.Chain:
-		b = *val.(*bytebuf.Chain)
+		b = *x
 	default:
 		ok = false
 	}
@@ -112,11 +112,11 @@ func ConvBytes(val any) (b []byte, ok bool) {
 // ConvBytesSlice tries to convert value to slice of bytes.
 func ConvBytesSlice(val any) (b [][]byte, ok bool) {
 	ok = true
-	switch val.(type) {
+	switch x := val.(type) {
 	case [][]byte:
-		b = val.([][]byte)
+		b = x
 	case *[][]byte:
-		b = *val.(*[][]byte)
+		b = *x
 	default:
 		ok = false
 	}
@@ -126,11 +126,11 @@ func ConvBytesSlice(val any) (b [][]byte, ok bool) {
 // ConvStr tries to convert value to string.
 func ConvStr(val any) (s string, ok bool) {
 	ok = true
-	switch val.(type) {
+	switch x := val.(type) {
 	case string:
-		s = val.(string)
+		s = x
 	case *string:
-		s = *val.(*string)
+		s = *x
 	default:
 		ok = false
 	}
@@ -140,11 +140,11 @@ func ConvStr(val any) (s string, ok bool) {
 // ConvStrSlice tries to convert value to string slice.
 func ConvStrSlice(val any) (s []string, ok bool) {
 	ok = true
-	switch val.(type) {
+	switch x := val.(type) {
 	case []string:
-		s = val.([]string)
+		s = x
 	case *[]string:
-		s = *val.(*[]string)
+		s = *x
 	default:
 		ok = false
 	}
@@ -154,11 +154,11 @@ func ConvStrSlice(val any) (s []string, ok bool) {
 // ConvBool tries to convert value ti boolean.
 func ConvBool(val any) (b bool, ok bool) {
 	ok = true
-	switch val.(type) {
+	switch x := val.(type) {
 	case bool:
-		b = val.(bool)
+		b = x
 	case *bool:
-		b = *val.(*bool)
+		b = *x
 	default:
 		ok = false
 	}
@@ -168,69 +168,69 @@ func ConvBool(val any) (b bool, ok bool) {
 // Convert interface value with arbitrary underlying type to integer value.
 func if2int(raw any) (r int64, ok bool) {
 	ok = true
-	switch raw.(type) {
+	switch x := raw.(type) {
 	case int:
-		r = int64(raw.(int))
+		r = int64(x)
 	case *int:
-		r = int64(*raw.(*int))
+		r = int64(*x)
 	case int8:
-		r = int64(raw.(int8))
+		r = int64(x)
 	case *int8:
-		r = int64(*raw.(*int8))
+		r = int64(*x)
 	case int16:
-		r = int64(raw.(int16))
+		r = int64(x)
 	case *int16:
-		r = int64(*raw.(*int16))
+		r = int64(*x)
 	case int32:
-		r = int64(raw.(int32))
+		r = int64(x)
 	case *int32:
-		r = int64(*raw.(*int32))
+		r = int64(*x)
 	case int64:
-		r = raw.(int64)
+		r = x
 	case *int64:
-		r = *raw.(*int64)
+		r = *x
 	case uint:
-		r = int64(raw.(uint))
+		r = int64(x)
 	case *uint:
-		r = int64(*raw.(*uint))
+		r = int64(*x)
 	case uint8:
-		r = int64(raw.(uint8))
+		r = int64(x)
 	case *uint8:
-		r = int64(*raw.(*uint8))
+		r = int64(*x)
 	case uint16:
-		r = int64(raw.(uint16))
+		r = int64(x)
 	case *uint16:
-		r = int64(*raw.(*uint16))
+		r = int64(*x)
 	case uint32:
-		r = int64(raw.(uint32))
+		r = int64(x)
 	case *uint32:
-		r = int64(*raw.(*uint32))
+		r = int64(*x)
 	case uint64:
-		r = int64(raw.(uint64))
+		r = int64(x)
 	case *uint64:
-		r = int64(*raw.(*uint64))
+		r = int64(*x)
 	case []byte:
-		if len(raw.([]byte)) > 0 {
-			r, _ = strconv.ParseInt(byteconv.B2S(raw.([]byte)), 0, 0)
+		if len(x) > 0 {
+			r, _ = strconv.ParseInt(byteconv.B2S(x), 0, 0)
 		}
 	case *[]byte:
-		if len(*raw.(*[]byte)) > 0 {
-			r, _ = strconv.ParseInt(byteconv.B2S(*raw.(*[]byte)), 0, 0)
+		if len(*x) > 0 {
+			r, _ = strconv.ParseInt(byteconv.B2S(*x), 0, 0)
 		}
 	case string:
-		if len(raw.(string)) > 0 {
-			r, _ = strconv.ParseInt(raw.(string), 0, 0)
+		if len(x) > 0 {
+			r, _ = strconv.ParseInt(x, 0, 0)
 		}
 	case *string:
-		if len(*raw.(*string)) > 0 {
-			r, _ = strconv.ParseInt(*raw.(*string), 0, 0)
+		if len(*x) > 0 {
+			r, _ = strconv.ParseInt(*x, 0, 0)
 		}
 	case *bytebuf.Chain:
-		if (*raw.(*bytebuf.Chain)).Len() > 0 {
-			r, _ = strconv.ParseInt((*raw.(*bytebuf.Chain)).String(), 0, 0)
+		if (*x).Len() > 0 {
+			r, _ = strconv.ParseInt((*x).String(), 0, 0)
 		}
 	case intConverter:
-		if i, err := raw.(intConverter).Int(); err == nil {
+		if i, err := x.Int(); err == nil {
 			return i, true
 		}
 	default:
