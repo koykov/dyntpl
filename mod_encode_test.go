fd02fd67ec03a5622779ae28387749a59dddd2ec
Konstantin Voykov <koykov0@gmail.com>
2026-08-10 23:45:56 +0300

    encode: cover with test/bench

--- mod_encode_test.go
+++ mod_encode_test.go
@@ -11,6 +11,11 @@ func TestModEncode(t *testing.T) {
 	t.Run("hexUint", func(t *testing.T) { testModWA(t, modArgs{"value": uint(math.MaxUint / 123456)}) })
 	t.Run("hexFloat", func(t *testing.T) { testModWA(t, modArgs{"value": math.Pi}) })
 
+	t.Run("octStr", func(t *testing.T) { testModWA(t, modArgs{"value": "lorem ipsum dolor sit..."}) })
+	t.Run("octInt", func(t *testing.T) { testModWA(t, modArgs{"value": -int(math.MaxInt - 5e12)}) })
+	t.Run("octUint", func(t *testing.T) { testModWA(t, modArgs{"value": uint(math.MaxUint / 123456)}) })
+	t.Run("octFloat", func(t *testing.T) { testModWA(t, modArgs{"value": math.Pi}) })
+
 	t.Run("binStr", func(t *testing.T) { testModWA(t, modArgs{"value": "lorem ipsum dolor sit..."}) })
 	t.Run("binInt", func(t *testing.T) { testModWA(t, modArgs{"value": -int(math.MaxInt - 5e12)}) })
 	t.Run("binUint", func(t *testing.T) { testModWA(t, modArgs{"value": uint(math.MaxUint / 123456)}) })
@@ -22,6 +27,11 @@ func BenchmarkModEncode(b *testing.B) {
 	b.Run("hexInt", func(b *testing.B) { benchModWA(b, modArgs{"value": -int(math.MaxInt - 5e12)}) })
 	b.Run("hexUint", func(b *testing.B) { benchModWA(b, modArgs{"value": uint(math.MaxUint / 123456)}) })
 	b.Run("hexFloat", func(b *testing.B) { benchModWA(b, modArgs{"value": math.Pi}) })
+
+	b.Run("octStr", func(b *testing.B) { benchModWA(b, modArgs{"value": "lorem ipsum dolor sit..."}) })
+	b.Run("octInt", func(b *testing.B) { benchModWA(b, modArgs{"value": -int(math.MaxInt - 5e12)}) })
+	b.Run("octUint", func(b *testing.B) { benchModWA(b, modArgs{"value": uint(math.MaxUint / 123456)}) })
+	b.Run("octFloat", func(b *testing.B) { benchModWA(b, modArgs{"value": math.Pi}) })
 
 	b.Run("binStr", func(b *testing.B) { benchModWA(b, modArgs{"value": "lorem ipsum dolor sit..."}) })
 	b.Run("binInt", func(b *testing.B) { benchModWA(b, modArgs{"value": -int(math.MaxInt - 5e12)}) })
